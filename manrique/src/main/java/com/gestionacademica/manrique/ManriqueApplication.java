0affd3e2fe0f697214e54e01f37d81a82126f308
Marco <mancomencho83@gmail.com>
2025-09-26 14:45:40 -0600

    INICIAMOS EL PROYECTO DE GESTION ACADEMICA

---
+++ manrique/src/main/java/com/gestionacademica/manrique/ManriqueApplication.java
@@ -0,0 +1,13 @@
+package com.gestionacademica.manrique;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManriqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManriqueApplication.class, args);
+	}
+
+}
